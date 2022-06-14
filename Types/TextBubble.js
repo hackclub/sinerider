202bfc2b8a9cf5d4c529bc7c4e85711575e7f3ec
Neel Redkar <neel.redkar@gmail.com>
2022-06-13 21:28:59 -0700

    add help bubble 'type'

---
+++ Types/TextBubble.js
@@ -0,0 +1,42 @@
+/**
+ * Create a text bubble which can be appended to a DOM Element
+ * @param {string} spec.content text in the bubble
+ * @param {string} spec.domSelector parent dom query selector
+ * @param {object} spec.style extra styles to be added like top/bottom/color
+ * @returns 
+ */
+function TextBubble(spec) {
+    const {
+      self
+    } = Entity(spec, 'TextBubble')
+  
+    const {
+      content = 'Hello',
+      domSelector,
+      style = {}
+    } = spec
+    
+    let {
+      visible
+    } = spec
+
+    const domElement = $(domSelector)
+    const helperBubble = document.createElement("div")
+    helperBubble.className = "helper-bubble"
+    helperBubble.innerHTML = content
+
+    helperBubble.style.display = visible ? "block" : "none"
+    Object.assign(helperBubble.style, style)
+
+    domElement.appendChild(helperBubble)
+    
+    function toggleVisible(newVis = !visible) {
+      visible = newVis
+      helperBubble.style.display = visible ? "block" : "none"
+    }
+
+    return self.mix({
+      toggleVisible,
+      helperBubble
+    })
+  }
