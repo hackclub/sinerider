4c343f22e18121484e7ee6935385d57734209877
Christopher Walker <15720465+polytroper@users.noreply.github.com>
2023-04-15 19:31:46 -0700

    Move cap to separate parameter and lower to 10

--- Types/Entities/Navigator.js
+++ Types/Entities/Navigator.js
@@ -44,15 +44,18 @@ function Navigator(spec) {
 
   const bubbles = _.map(levelData, createBubble)
   const bubbleRenderQueue = []
+  const bubbleRenderCap = 10
 
   function start() {
     if (initialBubble) initialBubble.completeAllRequirements()
   }
 
   function tick() {
-    let b = 0
-    while (b++ <= 30 && bubbleRenderQueue.length > 0)
-      bubbleRenderQueue.pop().render()
+    if (bubbleRenderQueue.length > 0) {
+      let b = 0
+      while (b++ <= bubbleRenderCap && bubbleRenderQueue.length > 0)
+        bubbleRenderQueue.pop().render()
+    }
   }
 
   function draw() {
@@ -179,7 +182,7 @@ function Navigator(spec) {
     for (bubble of bubbles) {
       if (bubble.visible && !bubble.rendered) {
         // We cap the number of bubbles rendered synchronously to prevent crashing on some iphones
-        if (b++ > 30) bubble.render()
+        if (b++ < bubbleRenderCap) bubble.render()
         else bubbleRenderQueue.push(bubble)
       }
     }
