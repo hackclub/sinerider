97df11a8cb914b886a760d7a08e78c75c5bf44af
ProfSucrose <kai@profsucrose.dev>
2023-07-11 10:40:01 -0400

    Refactor Asset loader to use blobs/file-sizes, add timeouts

--- Types/Assets.js
+++ Types/Assets.js
@@ -14,12 +14,88 @@ function Assets(spec) {
 
   if (callbacks.progress) callbacks.progress(0, loadTotal)
 
-  const ASSET_TIMEOUT = 5000
+  const maxRetryAttempts = 5
 
-  const assetsLoaded = {}
-  const attemptsTried = {}
+  let totalRetryAttempts = 0
+
+  // Start with 100 kbps for initial guess
+  const predictedNetworkSpeedBps = 100 * 1024
+
+  const assetTimeouts = []
+
+  let checkAssetTimeoutsInterval = null
 
   load(self)
+
+  function soundFromBlob(blob, assetSpec) {
+    const blobType = blob.type
+    const extension = blobType.substring(blobType.indexOf('/') + 1)
+    return new Promise((resolve, reject) => {
+      const blobUrl = Object.createObjectURL(blob)
+      assetSpec.src = blobUrl
+      const howl = new Howl({
+        ...assetSpec,
+        ext: extension,
+        onload: () => resolve(howl),
+        onerror: (error) => reject(error),
+      })
+    })
+  }
+
+  function imageFromBlob(blob, assetSpec) {
+    const blobUrl = Object.createObjectURL(blob)
+    const img = new Image()
+    img.src = blobUrl
+    return new Promise((resolve, reject) => {
+      img.onload = () => resolve(img)
+      img.onerror = (error) => reject(error)
+    })
+  }
+
+  function shaderFromBlob(blob, assetSpec) {
+    return blob.text()
+  }
+
+  function correctNetworkSpeedPrediction(bytes, loadTimeMs) {
+    const bps = bytes / loadTimeMs
+    predictedNetworkSpeedBps = 0.8 * predictedNetworkSpeedBps + 0.2 * bps
+  }
+
+  function loadAssetFromPath(object, path, assetFromBlob, assetSpec) {
+    const controller = new AbortController()
+
+    const abortControllers = assetAbortControllers[key] || []
+
+    abortControllers.push(controller)
+
+    const start = performance.now()
+
+    fetch(path, {
+      signal: controller.signal,
+    })
+      .then((response) => response.blob())
+      .then((blob) => {
+        // Incorporate blob load time + size to reestimate network speed
+        const now = performance.now()
+        correctNetworkSpeedPrediction(blob.size, now - start)
+        return blob
+      })
+      .then((blob) => assetFromBlob(blob, assetSpec))
+      .catch((error) => {
+        // Throw if assetFromBlob fails
+        handleFailure(error, path)
+      })
+      .then((asset) => {
+        // Abort other asset fetch attempts, if they exist
+        for (const c of abortControllers) {
+          if (c != controller) c.abort()
+        }
+
+        // Load asset
+        object[key] = asset
+        assetLoaded(path)
+      })
+  }
 
   function loadAsset(object, folders, file, key, assetSpec = {}) {
     // console.log(`Loading asset '${file}' from folders `, folders)
@@ -46,60 +122,61 @@ function Assets(spec) {
     const isSound = _.includes(soundExtensions, extension)
     const isShader = _.includes(shaderExtensions, extension)
 
-    let asset
+    const assetFromBlob = isImage
+      ? imageFromBlob
+      : isSound
+      ? soundFromBlob
+      : isShader
+      ? shaderFromBlob
+      : null
 
-    // TODO: Add fault tolerance for assets which fail to resolve
-    // (maybe add a timeout (w/ a max number of attempts?) for each asset type?)
+    if (!produceAsset) throw `Unexpected file extension: .${extension}`
 
-    if (isImage) {
-      asset = new Image()
-      fetch(path)
-        .then((res) => res.blob())
-        .then((blob) => {
-          asset.src = URL.createObjectURL(blob)
-          assetLoaded(path)
-        })
-        .catch((error) => {
-          handleFailure(error, path)
-        })
-    } else if (isSound) {
-      ;(assetSpec.src = path),
-        (asset = new Howl({
-          ...assetSpec,
-          onload: () => assetLoaded(path),
-          onloaderror: (error) => handleFailure(error, path),
-        }))
-    } else if (isShader) {
-      fetch(path)
-        .then((raw) => raw.text())
-        .then((text) => {
-          object[key] = text
-          assetLoaded(path)
-        })
-        .catch((error) => handleFailure(error, path))
-    } else {
-      throw `Unexpected file extension: .${extension}`
-    }
+    loadAssetFromPath(object, path, assetFromBlob, assetSpec)
 
-    // TODO: Clean up
-    setTimeout(() => {
+    // Max time it could reasonably take
+    // (estimate of how long it'd take to load 10 MB)
+
+    const assetTimeout = (timeoutMs) => {
       if (!assetsLoaded[path]) {
-        const attempts = attemptsTried[path] || 1
-        if (attempts < 5) {
-          console.log(`Failed to load ${path} after 5000ms, retrying...`)
-          attemptsTried[path] = attemptsTried + 1
-          loadAsset(arguments)
+        if (attemptsTried < maxRetryAttempts) {
+          console.log(
+            `Failed to load ${path} after ${timeoutMs}ms, retrying...`,
+          )
+          attemptsTried += 1
+          loadAssetFromPath(path, assetFromBlob)
+        } else {
+          handleFailure(error, path)
         }
       }
-    }, ASSET_TIMEOUT)
+    }
 
-    object[key] = asset
+    const now = performance.now()
+
+    assetTimeouts.push(assetTimeout, now)
 
     loadCount++
     loadTotal++
   }
 
+  function checkAssetTimeouts() {
+    const timeoutMs = ((10 * 1024 * 1024) / predictedNetworkSpeedBps) * 1000
+
+    const now = performance.now()
+
+    for (const [timeout, start] of assetTimeouts) {
+      const elapsedMs = now - start
+      if (elapsedMs > timeoutMs) {
+        timeout(timeoutMs)
+      }
+    }
+  }
+
   function load(object, folders = []) {
+    if (!checkAssetTimeoutsInterval) {
+      checkAssetTimeoutsInterval = setInterval(checkAssetTimeouts, 1000)
+    }
+
     _.each(object, (v, i) => {
       if (_.isObject(v)) {
         if (_.has(v, 'src')) loadAsset(object, folders, v.src, i, v)
@@ -112,6 +189,9 @@ function Assets(spec) {
     assetsLoaded[path] = true
     loadCount--
     if (loadCount == 0) {
+      clearInterval(checkAssetTimeoutsInterval)
+      checkAssetTimeoutsInterval = null
+
       callbacks.complete()
     } else if (callbacks.progress) {
       callbacks.progress(loadTotal - loadCount, loadTotal)
