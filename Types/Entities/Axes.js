061e9cc4b41d33f569ae86ae341339cde7238773
Christopher Walker <15720465+polytroper@users.noreply.github.com>
2024-01-16 16:30:12 -0800

    Cleanup

--- Types/Entities/Axes.js
+++ Types/Entities/Axes.js
@@ -11,28 +11,29 @@ function Axes(spec) {
   const screenOrigin = Vector2()
 
   function tick() {}
-  function drawLocal(){
-    const xTicks = Math.ceil(camera.upperRight.x-camera.lowerLeft.x)+1
-    const yTicks = Math.ceil(camera.upperRight.y-camera.lowerLeft.y)+1
+
+  function drawLocal() {
+    const xTicks = Math.ceil(camera.upperRight.x - camera.lowerLeft.x) + 1
+    const yTicks = Math.ceil(camera.upperRight.y - camera.lowerLeft.y) + 1
     ctx.beginPath()
 
     // Draw right x ticks
-    for (let i=0; i<xTicks; i++){
+    for (let i = 0; i < xTicks; i++) {
       ctx.moveTo(i, 0)
       ctx.lineTo(i, -0.2)
     }
     // Draw left x ticks
-    for (let i=0; i>-xTicks; i--){
+    for (let i = 0; i > -xTicks; i--) {
       ctx.moveTo(i, 0)
       ctx.lineTo(i, 0.2)
     }
     // Draw top y ticks
-    for (let i=0; i<yTicks; i++){
+    for (let i = 0; i < yTicks; i++) {
       ctx.moveTo(0, -i)
       ctx.lineTo(0.2, -i)
     }
     // Draw bottom y ticks
-    for (let i=0; i>-yTicks; i--){
+    for (let i = 0; i > -yTicks; i--) {
       ctx.moveTo(0, -i)
       ctx.lineTo(-0.2, -i)
     }
@@ -58,7 +59,6 @@ function Axes(spec) {
 
     ctx.stroke()
     camera.drawThrough(ctx, drawLocal, transform)
-    
   }
 
   return self.mix({
